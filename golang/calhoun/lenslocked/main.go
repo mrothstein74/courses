41a59606acaf5bf6ec67d295de0cd3c9977ec3e2
Michael Rothstein <michael.rothstein@circleci.com>
2022-10-03 14:09:18 -0400

    rendering the faq page with an html template

--- golang/calhoun/lenslocked/main.go
+++ golang/calhoun/lenslocked/main.go
@@ -37,19 +37,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-		<ul>
-			<li>Q: Is there a free version?</li>
-			<li>A: Yes! We offer a free trial for 30 days on any paid plans.</li>
-			<br>
-			<li>Q: What are your support hours?</li>
-			<li>A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</li>
-			<br>
-			<li>Q: How can I conttact support?</li>
-			<li>A: Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a></li>
-		</ul>
-		`)
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
