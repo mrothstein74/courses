8b781ff934a10b55b52e4ed52c713b059dd29f1a
Michael Rothstein <michael.rothstein@circleci.com>
2022-10-23 09:42:42 -0400

    using gorm to create users table

--- golang/calhoun/lenslocked/cmd/exp/main.go
+++ golang/calhoun/lenslocked/cmd/exp/main.go
@@ -1,133 +1,41 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/hellofresh/health-go/v4/checks/postgres"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	// _ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
-}
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "baloo"
+	password = "junglebook"
+	dbname   = "lenslocked"
+)
 
-func (Cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		Cfg.Host, Cfg.Port, Cfg.User, Cfg.Password, Cfg.Database, Cfg.SSLMode)
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
 }
 
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "baloo",
-		Password: "junglebook",
-		Database: "lenslocked",
-		SSLMode:  "disable",
-	}
-
-	db, err := sql.Open("pgx", cfg.String())
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+	sqlDB, err := db.DB()
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-	err = db.Ping()
-	if err != nil {
+	defer sqlDB.Close()
+	if err := sqlDB.Ping(); err != nil {
 		panic(err)
 	}
-	fmt.Println("Connected!")
 
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS users (
-			id	SERIAL PRIMARY KEY,
-			name TEXT,
-			email TEXT UNIQUE NOT NULL
-		);
-
-		CREATE TABLE IF NOT EXISTS orders (
-			id SERIAL PRIMARY KEY,
-			user_id INT NOT NULL,
-			amount INT,
-			description TEXT
-		);
-	`)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Tables created.")
-
-	// name := "New User"
-	// email := "mike_new@gmail.com"
-	// row := db.QueryRow(`
-	// 	INSERT INTO users(name, email)
-	// 	VALUES ($1, $2) RETURNING id;`, name, email)
-	// var id int
-	// err = row.Scan(&id)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("User created. id =", id)
-
-	// id := 9
-	// row := db.QueryRow(`
-	// 	SELECT name, email
-	// 	FROM users
-	// 	WHERE id=$1;`, id)
-	// var name, email string
-	// err = row.Scan(&name, &email)
-	// if err == sql.ErrNoRows {
-	// 	fmt.Println("Error, no rows")
-	// }
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("User information: name=%s, email=%s", name, email)
-
-	// userID := 1
-	// for i := 1; i <= 5; i++ {
-	// 	amount := i * 100
-	// 	desc := fmt.Sprintf("Fake order %d", i)
-	// 	_, err := db.Exec(`
-	// 		INSERT INTO orders(user_id, amount, description)
-	// 		VALUES($1, $2, $3)`, userID, amount, desc)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Println("Create fake orders")
-	// }
-
-	type Order struct {
-		ID          int
-		userID      int
-		Amount      int
-		Description string
-	}
-
-	var orders []Order
-	userID := 1
-	rows, err := db.Query(`
-		SELECT id, amount, description
-		FROM orders
-		WHERE user_id=$1`, userID)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var order Order
-		order.userID = userID
-		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
-		if err != nil {
-			panic(err)
-		}
-		orders = append(orders, order)
-	}
-
-	fmt.Println("Orders: ", orders)
+	//db.Migrator().DropTable(&User{})
+	//db.AutoMigrate(&User{})
 }
